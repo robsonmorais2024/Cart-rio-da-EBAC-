3e74933bb70a6d9910458263e569035312ab9688
robsonmorais2024 <robsondev2024@gmail.com>
2024-11-11 22:19:32 -0300

    Opção de saída
    incluso a opção de saída

--- Cartório da EBAC lição.c
+++ Cartório da EBAC lição.c
@@ -129,6 +129,7 @@ int main()
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Deletar nomes\n");
+        printf("\t4- Sair do sistema! \n\n");//sair do sistema
         printf("Opção: "); //fim do menu
             
         scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -147,6 +148,11 @@ int main()
 					
 			case 3:
 			deletar();// chamada de função
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema! \n\n");
+			return 0;
 			break;
 					
 			default:
